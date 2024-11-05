cef22457fa389c764b8fc1766e726dddb6df5d2a
Lee-IDLE <nidus0526@gmail.com>
2024-11-05 23:45:32 +0900

    웹소켓 객체를 전역 변수로 따로 빼는 중

---
+++ composeApp/src/commonMain/kotlin/org/lee/talk_to_we_client/objectClass/webSocket.kt
@@ -0,0 +1,66 @@
+package org.lee.talk_to_we_client.objectClass
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.websocket.WebSockets
+import io.ktor.client.plugins.websocket.webSocket
+import io.ktor.client.plugins.websocket.webSocketSession
+import io.ktor.http.HttpMethod
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.readText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.consumeEach
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+object webSocket {
+    var webSocketSession: WebSocketSession? = null
+    /*
+    receive 함수에서 서버로 부터 오는 모든 값을 받고,
+    map에 넣어뒀다가 각자 필요한 곳에서 꺼내 가는건 어떨까?
+    그 메시지가 어디꺼인지 아는 방법은
+    key값은 category로 하고
+    value를 Message 내용으로 하는거지
+    채팅 방 마다 구분도 필요하니까 Message 내용에 채팅방의 번호도 줘야 할 듯?
+     */
+
+    suspend fun connect(){
+        if(webSocketSession == null || !webSocketSession!!.isActive) {
+            try{
+                val client = HttpClient(CIO).config {
+                    install(WebSockets)
+                }
+
+                webSocketSession = client.webSocketSession(
+                    method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = ""
+                )
+
+                receive()
+            }catch (e: Error) {
+                println("webSocket connection fail: ${e.message}")
+            }
+
+        }
+    }
+
+    fun send(message: String) {
+        CoroutineScope(Dispatchers.IO).launch {
+            webSocketSession?.send(Frame.Text(message))
+        }
+    }
+
+    fun receive(){
+        CoroutineScope(Dispatchers.IO).launch {
+            webSocketSession?.incoming?.consumeEach { frame ->
+                when (frame) {
+                    is Frame.Text -> {
+                        println("Receive to Server: ${frame.readText()}")
+                    }
+                    else -> { println("EXT...") }
+                }
+            }
+        }
+    }
+}
--- composeApp/src/commonMain/kotlin/org/lee/talk_to_we_client/services/communicationManager.kt
+++ composeApp/src/commonMain/kotlin/org/lee/talk_to_we_client/services/communicationManager.kt
@@ -3,31 +3,16 @@ package org.lee.talk_to_we_client.services
 import androidx.lifecycle.ViewModel
 import io.ktor.client.*
 import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.HttpTimeout
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.timeout
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.websocket.webSocket
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.client.statement.request
-import io.ktor.http.ContentType
 import io.ktor.http.HttpMethod
-import io.ktor.http.content.Version
-import io.ktor.http.contentType
-import io.ktor.http.isSuccess
-import io.ktor.serialization.kotlinx.json.json
 import io.ktor.websocket.Frame
 import io.ktor.websocket.readText
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.channels.consumeEach
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.withContext
 import kotlinx.serialization.json.Json
 import org.lee.talk_to_we_client.models.LoginData
 import org.lee.talk_to_we_client.models.RequestData
 import org.lee.talk_to_we_client.viewModels.loginViewModel
-import java.net.ProtocolFamily
 
 class communicationManager {
     var viewModels: ViewModel? = null
@@ -53,7 +38,7 @@ class communicationManager {
             // 참고 https://java-jedi.medium.com/welcome-ktor-client-your-next-http-client-for-kotlin-based-project-part-ii-236462d4c836
             println("try connection")
             client.webSocket(
-                method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/login"
+                method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = ""
             ) {
                 println("connection success")
                 send(Frame.Text(jsonData))
